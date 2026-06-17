a3e27947ff918ae2a4f43bc9a5cbf72e5cea1be0
KaloyanJk <kaloyan112@gmail.com>
2026-06-17 20:05:22 +0100

    Add post model

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgres")
+    "DATABASE_URL", "postgresql://neondb_owner:npg_sbYDF81JnfWP@ep-wandering-bread-asa0xhgg.c-4.eu-central-1.aws.neon.tech/drone_coke_plaza_21431")
