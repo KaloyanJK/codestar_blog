89a26ac552d7702e2b5fefceeb6d7c218e4b07f1
KaloyanJk <kaloyan112@gmail.com>
2026-06-18 09:47:56 +0100

    Add Comments model

--- blog/models.py
+++ blog/models.py
@@ -45,7 +45,8 @@ class Ticket(models.Model):
         return f"Ticket for {self.ticket_holder}"
     
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.CharField(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
+    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
