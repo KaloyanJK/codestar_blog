7bc3a71cfaf21d6d1e885a69c56d8314daf5d4c7
KaloyanJk <kaloyan112@gmail.com>
2026-06-18 23:08:24 +0100

    Add draft/posted view for admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,12 @@ from .models import Post, Comment, Ticket, Event
 
 
 # Register your models here.
-admin.site.register(Post)
+#admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Ticket)
 admin.site.register(Event)
+
+#Show Published / Draft clearly in admin list
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "author", "created_on")
