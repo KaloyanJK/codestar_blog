710712e2ee880447edaa692ff4fc80a126cd63f9
KaloyanJk <kaloyan112@gmail.com>
2026-06-19 08:21:57 +0100

    remove env

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_sbYDF81JnfWP@ep-wandering-bread-asa0xhgg.c-4.eu-central-1.aws.neon.tech/drone_coke_plaza_21431")
