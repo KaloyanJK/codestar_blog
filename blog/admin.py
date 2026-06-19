c0e55912b7106e05de06e479ad1cc3c734f2cb35
KaloyanJk <kaloyan112@gmail.com>
2026-06-19 09:09:33 +0100

    add field to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from .models import Post, Comment, Ticket, Event
+from django_summernote.admin import SummernoteModelAdmin
 
 
+#Show Published / Draft clearly in admin list
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", 'slug', "author", "created_on")
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+    
 # Register your models here.
 #admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Ticket)
 admin.site.register(Event)
 
-#Show Published / Draft clearly in admin list
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "status", "author", "created_on")
--- codestar/urls.py
+++ codestar/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"), name="blog-urls"),
+    path('summernote/', include('django_summernote.urls')),
     #path('blog/', blog_views.my_blog, name="blog"), # the app urls are loaded as the main urls
+    path("", include("blog.urls"), name="blog-urls"),
 ]
