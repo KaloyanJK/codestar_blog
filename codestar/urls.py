60de7cabcd75c27389b6ca2ca16146e3f27ea3cc
KaloyanJk <kaloyan112@gmail.com>
2026-06-21 19:10:02 +0100

    Add about page

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('about/', include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    #path('blog/', blog_views.my_blog, name="blog"), # the app urls are loaded as the main urls
     path("", include("blog.urls"), name="blog-urls"),
 ]
